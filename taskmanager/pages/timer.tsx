bef4432c0741fe3146cbe3f5e7412d0fc7a75ca6
diexiy <mariabiirtm@gmail.com>
2025-09-29 06:18:45 +0200

    skiss på timer

--- taskmanager/pages/timer.tsx
+++ taskmanager/pages/timer.tsx
@@ -3,7 +3,8 @@
 import { useEffect, useState } from "react";
 import {motion} from "framer-motion";
 
-"use Client"
+"use Client" // säger till Next.js that detta är bara till för de som använder sidan.
+            // finns ingenting på servern som görs
 
 
 
@@ -11,42 +12,50 @@ import {motion} from "framer-motion";
 export default function Home() {
 
     // settting = to after the different modes is the default state
-    const [activeMode, setActiveMode] = useState("Timer");
-    const [timeLeft, setTimeLeft] = useState(25 * 60);
-    const [isRunning, setIsRunning] = useState(false);
-    const [showSetting, setShowSettings] = useState(false);
+    const [activeMode, setActiveMode] = useState("Timer"); // acriveMode sparar den mode jag är i
+    const [timeLeft, setTimeLeft] = useState(25 * 60); // har koll på hur många sekunder jag har kvar 
+    const [isRunning, setIsRunning] = useState(false); // är timern på eller av?
+    const [showSetting, setShowSettings] = useState(false); // tror ej jag kommer använda denna
     const [durations, setDurations] = useState ({
-        Timer: 1 * 60,
+        Timer: 0.2 * 60,
         "short break": 5 * 60,
-        "long break": 15*60,
-    })
+        "long break": 15 * 60,
+    }) // sparar hur långa alla mode är i sekunder
 
 
-    const modes = ["Timer", "short break", "long break" ]
+    const modes = ["Timer", "short break", "long break" ] // en lista med de olika modes
+
+
+    /* runs whenever isRunning changes
+    if running --> creates a timer that subtracts 1 from timeLeft every second
+    if paused --> clears the interval.
+    return makes sure we clean up the interval when the component re-renders */
 
     useEffect(() => {
         let interval : NodeJS.Timeout;
 
         if(isRunning) {
-            interval = setInterval(() => {
-                setTimeLeft((prev) => (prev > 0 ? prev -1 : 0));
-            }, 1000)
-        } else {
-            clearInterval(interval);
-        } 
+                 interval = setInterval(() => {
+                        setTimeLeft((prev) => (prev > 0 ? prev -1 : 0));
+                    }, 1000)
+                     }  else {
+                             clearInterval(interval);
+                             } 
+            return() => clearInterval(interval);
+     }, [isRunning]);
 
-
-        return() => clearInterval(interval);
-
-    }, [isRunning]);
-
-        //reset the timer when the mode changers
-
+        
+        /* whenever you switch mode, reset timeLeft to the modes duration
+        for example: switching to short break sets the time to 5 * 60 */ 
         useEffect(() => {
             setTimeLeft(durations[activeMode])
         }, [activeMode, durations]);
 
 
+
+
+        /*if the timer hits zero then we wait one second, and then we switch to the next mode
+        i might wanna remove this. */ 
         useEffect(() => {
 
             if(timeLeft === 0) {
