cb71e27fd0a4864f7d4bdc97c17305b7beecdea0
ZweeDaLegend <91790736+ZweeDaLegend@users.noreply.github.com>
2025-10-08 22:17:54 +0200

    Fixed typo

--- taskmanager/pages/notesList.tsx
+++ taskmanager/pages/notesList.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { Note, NoteContent} from "@/components/NoteCompontent";
+import { Note, NoteContent} from "@/components/NoteComponent";
 
 export default function NotesListPage() {
   const [notes, setNotes] = useState<Note[]>([]);
