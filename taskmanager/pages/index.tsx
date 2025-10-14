208ea61be6fad5c7b83a9c420c552d2a5a6a0742
ZweeDaLegend <91790736+ZweeDaLegend@users.noreply.github.com>
2025-10-14 03:01:08 +0200

    Mostly done with todos

--- taskmanager/pages/index.tsx
+++ taskmanager/pages/index.tsx
@@ -8,6 +8,10 @@ export default function Home() {
     window.location.href = "/login";
   };
 
+  const handleDashboard = () => {
+    window.location.href = "/dashboard";
+  };
+
   return (
     <main className="relative w-full h-screen overflow-hidden bg-gradient-to-br from-purple-900 via-black to-black">
 
@@ -26,9 +30,16 @@ export default function Home() {
 
         <button
           onClick={handleSignUp}
-          className="w-40 py-3 bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 transition"
+          className="w-40 mb-4 py-3 bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 transition"
         >
           Register
+        </button>
+
+        <button
+          onClick={handleDashboard}
+          className="w-40 py-3 bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 transition"
+        >
+          Todo Dashboard
         </button>
       </div>
     </main>
